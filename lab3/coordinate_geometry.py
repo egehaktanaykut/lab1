5e47b70e98553b819b74d77dd27b56ff9fe180e3
Ege Haktan Aykut <aykutegehaktan@gmail.com>
2026-02-27 16:11:36 +0300

    lab1.3

---
+++ lab3/coordinate_geometry.py
@@ -0,0 +1,6 @@
+x1 = float(input("Enter the x-coordinate of the first point: "))
+y1 = float(input("Enter the y-coordinate of the first point: "))
+x2 = float(input("Enter the x-coordinate of the second point: "))
+y2 = float(input("Enter the y-coordinate of the second point: "))
+distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+print(f"The distance between the points ({x1}, {y1}) and ({x2}, {y2}) is: {distance}")
